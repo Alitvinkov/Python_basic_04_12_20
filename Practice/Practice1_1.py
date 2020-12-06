3f27251d975ce4840f73f7d2fb2f4c2c549f70ac
Anton <ant_litv@icloud.com>
2020-12-06 16:01:52 +0100

    Practice1_1

---
+++ Practice/Practice1_1.py
@@ -0,0 +1,6 @@
+user_name = input('Vvedite vawe imya:\n>>> ')
+user_age = input('Vvedite vaw vozrast:\n>>> ')
+user_genus = input('Vvedite vaw pol:\n>>> ')
+
+sys_message = f'Polzovatel: {user_name} \n vozrastom {user_age} \n pol {user_genus} \n zaregestrirovan v sisteme'
+print(sys_message)
