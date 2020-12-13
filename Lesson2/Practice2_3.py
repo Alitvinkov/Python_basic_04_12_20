d6df442bf8d081ddcd4b3002eaa3a2dd38f9595f
Anton <ant_litv@icloud.com>
2020-12-13 13:38:21 +0100

    Practice2_3.py

---
+++ Lesson2/Practice2_3.py
@@ -0,0 +1,22 @@
+# 3. Пользователь вводит месяц в виде целого числа от 1 до 12.
+# Сообщить к какому времени года относится месяц (зима, весна, лето, осень).
+# Напишите решения через list и через dict.
+year_list = ['Zima', 'Vesna', 'Leto', 'Osen']
+year_dict = {
+    0: 'Zima',
+    1: 'Vesna',
+    2: 'Leto',
+    3: 'Osen',
+}
+month = ()
+
+while True:
+    month = input('Vvedite nomer mesyaca v vide celogo chisla ot 1 - 12 :\n>>>')
+    if month.isdigit():
+        month = int(month)
+        break
+    else:
+        print('Vi vveli neverni format')
+
+print('Po spisku: ' + year_list[month // 3])
+print('Po slovaru: ' + year_dict.get(month // 3))
